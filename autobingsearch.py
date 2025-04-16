8669d42c96c5260d8ebeaf8f4cb7e8bbff74d066
Rafael <156742973+RafaelDGP@users.noreply.github.com>
2025-04-16 03:58:43 -0300

    Update autobingsearch.py
    Better performance and added a way to start and stop without needing to open the compiler.

--- autobingsearch.py
+++ autobingsearch.py
@@ -4,23 +4,66 @@ import pyautogui
 import keyboard
 
 click_interval = 5
+active = False
+close = False
+
+def get_list_of_words(path):
+    with open(path, 'r', encoding='utf-8') as f:
+        return f.read().splitlines()
+
+words = get_list_of_words('words.txt')
+
+print("Press 'Q' to start and 'E' to stop")
 
 while True:
-    current_x, current_y = pyautogui.position()
-    pyautogui.click(current_x, current_y)
-    time.sleep(click_interval)
-    def get_list_of_words(path):
-        with open(path, 'r', encoding='utf-8') as f:
-            return f.read().splitlines()
-        
-    words = get_list_of_words('words.txt')
-    random_word = random.choice(words)
-    output = random.sample(random_word, len(random_word))
-    pyautogui.typewrite(output, interval=0.05)
-    keyboard.send("enter")
-    time.sleep(2)
-    
-    pyautogui.doubleClick(current_x, current_y)
-    keyboard.send("backspace")
-    
-    time.sleep(1)
+    # Starts bot
+    if keyboard.is_pressed('q') and not active:
+        active = True
+        time.sleep(0.5)
+
+    if active:
+        # Checks if user wants to stop before act
+        if keyboard.is_pressed('e'):
+            active = False
+            time.sleep(0.5)
+            continue
+
+        # Execute automatic tasks
+        current_x, current_y = pyautogui.position()
+        pyautogui.click(current_x, current_y)
+
+        # While wait, checks if needs to stop
+        for _ in range(int(click_interval * 10)):
+            if keyboard.is_pressed('e'):
+                active = False
+                time.sleep(0.5)
+                break
+            time.sleep(0.1)
+        if not active:
+            continue
+
+        # Chooses a random word from the words.txt file and scrambles the chosen word, then press enter.
+        random_word = random.choice(words)
+        scrambled = random.sample(random_word, len(random_word))
+        pyautogui.typewrite(scrambled, interval=0.05)
+        keyboard.send("enter")
+
+        for _ in range(20):
+            if keyboard.is_pressed('e'):
+                active = False
+                time.sleep(0.5)
+                break
+            time.sleep(0.1)
+        if not active:
+            continue
+
+        # Select all text and exclude
+        pyautogui.doubleClick(current_x, current_y)
+        keyboard.send("backspace")
+
+        for _ in range(10):
+            if keyboard.is_pressed('e'):
+                active = False
+                time.sleep(0.5)
+                break
+            time.sleep(0.1)
